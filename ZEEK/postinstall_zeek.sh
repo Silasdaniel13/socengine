69854619e2ac874a6d554aafd4311850a76817cf
Silasdaniel13 <50920787+Silasdaniel13@users.noreply.github.com>
2022-04-20 03:11:17 +0200

    #181726040 bash scripts to install Zeek on Debian
    There is one install script and one postinstall script. The postinstall script receive two informations in parameter (the server hostname and the network interface to be monitored)

---
+++ ZEEK/postinstall_zeek.sh
@@ -0,0 +1,90 @@
+#!/bin/bash
+#
+# uvdesk.sh
+#
+# This script is used to install Zeek IDS on a Debian based server 
+# Authors:
+#  Silas VIGAN <sisedaro05@yahoo.fr>
+#
+# 
+
+
+#sourcelist=./sources.list
+#conf=./uvdesk.conf
+
+install_date=$(date --rfc-3339=date)
+logfile=/opt/socenngine/logs/zeek_postinstall_$creating_date.log
+install_home=/opt/socenngine/ZEEK/
+node_config_file=/opt/zeek/etc/node.cfg
+touch $logfile
+server_host_name=$1
+server_interface=$2
+
+
+
+
+####################################################
+#            Installing Prerequisites              #
+####################################################
+
+
+cat <<BAN  
+#################################################### 
+#         Zeek system files configuration          #
+####################################################
+BAN
+
+echo "**************************************Zeek node file configuration started*****************"
+ 
+ echo $(date --rfc-3339=seconds) >> $logfile
+ echo "**************************************Zeek node file configuration started*****************" >> $logfile
+
+
+
+
+
+/opt/zeek/bin/zeekctl stop
+
+echo "**************************************Zeek service stopped to edit the config file*****************"
+ 
+ echo $(date --rfc-3339=seconds) >> $logfile
+ echo "**************************************Zeek service stopped to edit the config file*****************" >> $logfile
+
+
+
+if (sed -i 's/host=/host=$server_host_name/' $node_config_file);
+then
+	echo "**************************************Zeek hostname successfully updated*****************"
+ 
+ echo $(date --rfc-3339=seconds) >> $logfile
+ echo "**************************************Zeek hostname successfully updated*****************" >> $logfile
+
+else
+	echo "**************************************Zeek hostname unsuccessfully updated*****************"
+ 
+ echo $(date --rfc-3339=seconds) >> $logfile
+ echo "**************************************Zeek hostname unsuccessfully updated*****************" >> $logfile
+fi
+
+
+
+
+
+if (sed -i 's/interface=/interface=$server_interface/' $node_config_file);
+then
+	echo "**************************************Zeek server interface successfully updated*****************"
+ 
+ echo $(date --rfc-3339=seconds) >> $logfile
+ echo "**************************************Zeek server interface successfully updated*****************" >> $logfile
+
+else
+	echo "**************************************Zeek server interface unsuccessfully updated*****************"
+ 
+ echo $(date --rfc-3339=seconds) >> $logfile
+ echo "**************************************Zeek server interface unsuccessfully updated*****************" >> $logfile
+fi
+	
+
+
+
+exit 0
--- ZEEK/zeek.sh
+++ ZEEK/zeek.sh
@@ -15,8 +15,16 @@
 install_date=$(date --rfc-3339=date)
 logfile=/opt/socenngine/logs/zeek_$creating_date.log
 install_home=/opt/socenngine/ZEEK/
-touch $logfile
-mkdir $install_home
+post_install=./postinstall_zeek.sh
+
+if (test -d $install_home);
+then
+	rm -r $install_home/*
+else
+	touch $logfile
+	mkdir $install_home
+fi
+
 debian_version=Debian_10
 
 
@@ -36,8 +44,21 @@ BAN
 
 sudo apt-get install -y cmake make gcc g++ flex bison libpcap-dev libssl-dev python3 python3-dev swig zlib1g-dev 
 
+echo "**************************************Tools installed successfully*****************"
+
+ 
+  echo $(date --rfc-3339=seconds) >> $logfile
+  echo "**************************************Tools installed successfully*****************" >> $logfile
+  
+  
 sudo apt-get install -y python3-git python3-semantic-version
 
+echo "**************************************Python successfully installed*****************"
+
+ 
+  echo $(date --rfc-3339=seconds) >> $logfile
+  echo "**************************************Python successfully installed*****************" >> $logfile
+  
 
 ####################################################
 #               Installing Zeek                    #
@@ -51,8 +72,31 @@ BAN
 
 echo 'deb http://download.opensuse.org/repositories/security:/zeek/Debian_10/ /' | sudo tee /etc/apt/sources.list.d/security:zeek.list
 curl -fsSL https://download.opensuse.org/repositories/security:zeek/Debian_10/Release.key | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/security_zeek.gpg > /dev/null
+
+echo "**************************************Zeek installing files successfully downloaded*****************"
+ 
+ echo $(date --rfc-3339=seconds) >> $logfile
+ echo "**************************************Zeek installing files successfully downloaded*****************" >> $logfile
+
 sudo apt update
-sudo apt install zeek-lts
+
+
+if (sudo apt install zeek-lts);
+then
+	echo "**************************************Zeek installing files     successfully downloaded*****************"
+ 
+        echo $(date --rfc-3339=seconds) >> $logfile
+        echo "**************************************Zeek installing files successfully downloaded*****************" >> $logfile
+fi
+
+
+
 /opt/zeek/bin/zeekctl install
+echo "**************************************Zeekctl Service successfully installed*****************"
+ 
+ echo $(date --rfc-3339=seconds) >> $logfile
+ echo "**************************************Zeekctl Service successfully installed*****************" >> $logfile
 
+ 
 
+exit 0
