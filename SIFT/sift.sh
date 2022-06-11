03ee33a329de8198053be31d906bce0211574aeb
Silasdaniel13 <50920787+Silasdaniel13@users.noreply.github.com>
2022-06-11 12:39:12 +0200

    SIFT Lateste Update

--- SIFT/sift.sh
+++ SIFT/sift.sh
@@ -36,29 +36,14 @@ curl -Lo /usr/local/bin/sift-cli-linux.sha256 https://github.com/sans-dfir/sift-
 
 curl -Lo /usr/local/bin/sift-cli-linux.sha256.asc https://github.com/sans-dfir/sift-cli/releases/download/v1.10.0/sift-cli-linux.sha256.asc
 
-#cp ./suricata /etc/default/suricata
+
 
 #Verification of the downloaded files
-#apt-get install -y gnupg
-#gpg --keyserver pgp.mit.edu --recv-keys 22598A94
-
-#cp ./suricata.yaml /etc/suricata/suricata.yaml
-#cp ./suricata.rules /etc/suricata/rules/suracata.rules
+apt-get install -y gnupg
+gpg --keyserver pgp.mit.edu --recv-keys 22598A94
 
 
 
 
-##################Update commands after editing some files 
-#suricata-update
-#suricata-update update-sources
-#suricata-update enable-source oisf/trafficid
-#suricata-update enable-source etnetera/aggressive
-#suricata-update enable-source sslbl/ssl-fp-blacklist
-#suricata-update enable-source et/open
-#suricata-update enable-source tgreen/hunting
-#suricata-update enable-source sslbl/ja3-fingerprints
-#suricata-update enable-source ptresearch/attackdetection
-#suricata-update
 
-#systemctl enable suricata
-#systemctl start suricata
+
